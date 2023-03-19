c708730aaa9012a8b81d3e691b25842e639dd1c9
Marco Catapano <marco.catapano@traderepublic.com>
2023-03-19 18:58:29 +0100

    Introduce anyhow dependency

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,76 @@ mod data;
 
 use crate::data::inbound::InstrumentChangeMessage;
 use crate::data::outbound::InstrumentUniverseChangeSet;
+use anyhow::Context;
 use elasticsearch::auth::Credentials;
 use elasticsearch::http::transport::{SingleNodeConnectionPool, TransportBuilder};
 use elasticsearch::http::Url;
 use elasticsearch::{Elasticsearch, UpdateParts};
 use futures_lite::stream::StreamExt;
-use lapin::{options::*, types::FieldTable, Connection, ConnectionProperties, Result};
+use lapin::{options::*, types::FieldTable, Connection, ConnectionProperties, Consumer};
 use serde_json::Value;
-
-use tokio::runtime::Runtime;
-
+use std::time::Duration;
+use tokio::time::timeout;
 use tokio_executor_trait::Tokio;
 
-async fn tokio_main() -> Result<()> {
-    let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut consumer = build_rabbitmq_consumer()
+        .await
+        .with_context(|| "Unable to build rabbitmq consumer")?;
+
+    let client =
+        build_elasticsearch_client().with_context(|| "Unable to build elasticsearch client")?;
+
+    while let Ok(Some(Ok(delivery))) =
+        timeout(Duration::from_millis(1000u64), consumer.next()).await
+    {
+        let delivered = &delivery.data;
+        let data = std::str::from_utf8(delivered).with_context(|| {
+            format!(
+                "Cannot decode delivered bytes from rabbitMQ: {:?}",
+                delivery.properties.message_id()
+            )
+        })?;
+        let change: InstrumentChangeMessage = serde_json::from_str(data)
+            .with_context(|| format!("Received data wasn't deserialisable: {}", data))?;
+        let change_set: InstrumentUniverseChangeSet = change.data.into();
+        let serialised_change_set = serde_json::to_string(&change_set).with_context(|| {
+            format!(
+                "Failed to serialise changeset for instrument {}",
+                &change_set.isin
+            )
+        })?;
+        let mut request: serde_json::Map<String, Value> = serde_json::Map::new();
+        request.insert("doc_as_upsert".to_owned(), Value::Bool(true));
+        request.insert(
+            "doc".to_owned(),
+            Value::Object(serde_json::from_str(&serialised_change_set)?),
+        );
+
+        let json: serde_json::Map<String, Value> = client
+            .update(UpdateParts::IndexId(
+                "instruments",
+                change_set.isin.as_str(),
+            ))
+            .body(request)
+            .send()
+            .await?
+            .json()
+            .await?;
+        println!("{:?}", json);
+        delivery.ack(BasicAckOptions::default()).await?;
+    }
+    Ok(())
+}
+
+async fn build_rabbitmq_consumer() -> anyhow::Result<Consumer> {
+    let addr = "amqp://127.0.0.1:5672/%2f";
     let tokio = Tokio::current();
     let conn =
-        Connection::connect(&addr, ConnectionProperties::default().with_executor(tokio)).await?;
+        Connection::connect(addr, ConnectionProperties::default().with_executor(tokio)).await?;
     let channel = conn.create_channel().await?;
-    let url = Url::parse("http://localhost:9200").unwrap();
-    let conn_pool = SingleNodeConnectionPool::new(url);
-    let transport = TransportBuilder::new(conn_pool)
-        .auth(Credentials::Basic(
-            "elastic".to_owned(),
-            "123test".to_owned(),
-        ))
-        .disable_proxy()
-        .build()
-        .unwrap();
-    let client = Elasticsearch::new(transport);
-
-    let mut consumer = channel
+    let consumer = channel
         .basic_consume(
             "instrumentsearch.platform",
             "rusterino",
@@ -40,38 +79,19 @@ async fn tokio_main() -> Result<()> {
             FieldTable::default(),
         )
         .await?;
-    println!("Start consoom");
-    while let Some(delivery) = consumer.next().await {
-        let actual_delivery = delivery.unwrap();
-        let delivered = &actual_delivery.data;
-        let data = std::str::from_utf8(delivered).unwrap();
-        let change: InstrumentChangeMessage = serde_json::from_str(data).unwrap();
-        let change_set: InstrumentUniverseChangeSet = change.data.into();
-        let serialised_change_set = serde_json::to_string_pretty(&change_set).unwrap();
-        let mut request: serde_json::Map<String, Value> = serde_json::Map::new();
-        request.insert("doc_as_upsert".to_owned(), Value::Bool(true));
-        request.insert(
-            "doc".to_owned(),
-            Value::Object(serde_json::from_str(&serialised_change_set).unwrap()),
-        );
-
-        let response = client
-            .update(UpdateParts::IndexId(
-                "instruments",
-                change_set.isin.as_str(),
-            ))
-            .body(request)
-            .send()
-            .await
-            .unwrap();
-        let json: serde_json::Map<String, Value> = response.json().await.unwrap();
-        println!("{:?}", json);
-        actual_delivery.ack(BasicAckOptions::default()).await?;
-    }
-    Ok(())
+    Ok(consumer)
 }
 
-fn main() {
-    let rt = Runtime::new().expect("failed to create runtime");
-    rt.block_on(tokio_main()).expect("error");
+fn build_elasticsearch_client() -> anyhow::Result<Elasticsearch> {
+    let url = Url::parse("http://localhost:9200")?;
+    let conn_pool = SingleNodeConnectionPool::new(url);
+    let transport = TransportBuilder::new(conn_pool)
+        .auth(Credentials::Basic(
+            "elastic".to_owned(),
+            "123test".to_owned(),
+        ))
+        .disable_proxy()
+        .build()?;
+
+    Ok(Elasticsearch::new(transport))
 }
